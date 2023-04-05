a0af4c49208c5da9c98153f8fd93c1c784c7e24a
Hemasku <nhtranaero@gmail.com>
2023-04-05 03:06:22 -0400

    Mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,12 @@ scrollUp.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+$(".tooltip").click(function () {
+    var $title = $(this).find(".title-info");
+    if (!$title.length) {
+        $(this).append('<span class="title-info">' + $(this).attr("title") + '</span>');
+    } else {
+        $title.remove();
+    }
+});
